b94eda66c1aa22aa8bf2a8c40a92d558324dd3f6
Aaron Kao <cckaron28@gmail.com>
2021-11-01 17:45:39 +0800

    * create DB migration files

---
+++ migrations/20211101074012_create_user-related_table.js
@@ -0,0 +1,48 @@
+
+exports.up = function(knex) {
+    return knex.schema
+        .createTable('organizations', function (table) {
+            table.increments('id');
+            table.string('name', 255).notNullable();
+        })
+        .createTable('departments', function (table) {
+            table.increments('id');
+            table.integer('org_id')
+                .unsigned().references('id')
+                .inTable('organizations')
+                .onDelete('SET NULL')
+                .index();
+            table.string('name', 255).notNullable();
+        })
+        .createTable('skins', function (table){
+            table.increments('id');
+            table.string('name').notNullable();
+            !table.integer('price').notNullable();
+        })
+        .createTable('users', function (table) {
+            table.increments('id');
+            table.integer('org_id')
+                .unsigned().references('id')
+                .inTable('organizations')
+                .onDelete('SET NULL')
+                .index();
+            table.integer('default_skin_id')
+                .unsigned().references('id')
+                .inTable('skins')
+                .onDelete('SET NULL')
+                .index();
+            table.string('profileImgUrl', 255).notNullable();
+            table.string('name', 64).notNullable();
+            table.string('email', 255).notNullable();
+            table.string('password', 255).notNullable();
+            table.enum('status', ['activated', 'deactivated', 'blocked']).defaultTo('activated');
+        })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTable("organizations")
+        .dropTable("departments")
+        .dropTable("skins")
+        .dropTable("users");
+};
